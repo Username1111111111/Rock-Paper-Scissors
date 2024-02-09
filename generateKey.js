8060bc18f8bdf081b95ad84ba37dd992d38f1444
Username <14928811+Username1111111111@users.noreply.github.com>
2024-02-09 16:27:58 +0400

    fix literals

--- generateKey.js
+++ generateKey.js
@@ -1,8 +1,9 @@
 // The script generates a cryptographically strong random key. (SecureRandom, RandomNumberGenerator, etc. - mandatory) with a length of at least 256 bits, makes own (computer's) move.
 const crypto = require("crypto");
 
+const KEY_SIZE = 32;
 // Generate a 256-bit random key
-const key = crypto.randomBytes(32);
+const key = crypto.randomBytes(KEY_SIZE);
 
 // Convert the key to hexadecimal string
 const keyHex = key.toString('hex');
